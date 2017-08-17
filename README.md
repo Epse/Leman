d077540e95da1f9689db445137634cc2d2916d82
Stef Pletinck <s@stefpletinck.be>
2017-08-17 13:11:33 +0200

    Initial commit with base backend framework

---
+++ README.md
@@ -0,0 +1,2 @@
+# Leman
+A web-based cross-platform rental management system with deep tracking abilities.
---
+++ backend/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// This function handles all requests to /items/operation
+// operation is one of the following: list, instock, rented
+// list gives all items, instock all those not rented and rented all those rented out
+func itemListHandler(w http.ResponseWriter, r *http.Request) {
+	operation := string(r.URL.Path[len("/items/")])
+	operation = strings.TrimSuffix(operation, "/")
+
+	if strings.Contains(operation, "/") {
+		w.WriteHeader(http.StatusBadRequest)
+		//TODO: more informative error message
+		fmt.Fprintf(w, "400 BAD REQUEST.")
+	}
+
+	if operation == "list" {
+		//TODO: return all items in neat JSON
+		panic("List operation not implemented")
+	} else if operation == "instock" {
+		//TODO: return all items in stock in neat JSON
+		panic("instock op not implemented")
+	} else if operation == "rented" {
+		//TODO: return all rented items as neat JSON
+		panic("rented op not implemented")
+	}
+}
+
+func itemViewHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+	fmt.Fprintf(w, "WIP")
+}
+
+func itemUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+	fmt.Fprintf(w, "WIP")
+}
+
+func itemNewHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+	fmt.Fprintf(w, "WIP")
+}
+
+func main() {
+	http.HandleFunc("/items/", itemListHandler)
+	http.HandleFunc("/item/view/", itemViewHandler)
+	http.HandleFunc("/item/update/", itemUpdateHandler)
+	http.HandleFunc("/item/new/", itemNewHandler)
+	http.ListenAndServe(":8080", nil)
+}
